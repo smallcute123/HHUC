593a0fb0570698dd09e2ee435ed9dd7b8ea360e8
chengxiaoyu <836849371@qq.com>
2019-01-18 17:07:12 +0800

    再在上传

--- miniprogram/pages/social/social.js
+++ miniprogram/pages/social/social.js
@@ -1,66 +1,166 @@
 // pages/social/social.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    userInfo: {},
+    menuList: {},//菜单集合
+    animationData: {},
+    startPoint: {},//触摸开始
+    dotPoint: {},//圆点坐标
+    startAngle: 0,//开始角度
+    tempAngle: 0,//移动角度
+    downTime: 0,//按下时间
+    upTime: 0,//抬起时间
+    // isRunning: false,//正在滚动
+  },
+  showName: function (e) {
+    var name = e.currentTarget.dataset.name;
+
+    console.log(name);
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({ 
+        userInfo: userInfo,
+      })
+    })
+    wx.getSystemInfo({
+      success: function (res) {
+        var windowWidth = res.windowWidth * 0.5;
+        that.setData({
+          //圆点坐标,x为屏幕一半,y为半径与margin-top之和,px
+          //后面获取的触摸坐标是px,所以这里直接用px.
+          dotPoint: { clientX: windowWidth, clientY: 250 }
+        })
+      }
+    })
+  },
+  onReady: function (e) {
+    var that = this;
+    app.menuConfig = {
+      menu: [
+        { 'index': 0, 'menu': '表白墙', 'src': './../../images/icon/表白墙.png','url':'love' },
+        { 'index': 1, 'menu': '跳骚市场', 'src': './../../images/icon/跳骚市场.png' },
+        { 'index': 2, 'menu': '校园新闻', 'src': './../../images/icon/校园新闻.png' },
+        { 'index': 3, 'menu': '敬请期待', 'src': './../../images/icon/更多.png' },
+        { 'index': 4, 'menu': '敬请期待', 'src': './../../images/icon/更多.png' },
+        { 'index': 5, 'menu': '敬请期待', 'src': './../../images/icon/更多.png' }
+      ]
+    }
+    // 绘制转盘
+    var menuConfig = app.menuConfig.menu,
+      len = menuConfig.length,
+      menuList = [],
+      degNum = 360 / len  // 文字旋转 turn 值
+    for (var i = 0; i < len; i++) {
+      menuList.push({ deg: i * degNum, menu: menuConfig[i].menu, src: menuConfig[i].src });
+      console.log("menu:" + menuConfig[i].menu)
+    }
+    that.setData({
+      menuList: menuList
+    });
+  },
+  // 菜单拖动的三个方法
+  buttonStart: function (e) {
+    this.setData({
+      startPoint: e.touches[0]
+    })
+    var x = this.data.startPoint.clientX - this.data.dotPoint.clientX;
+    var y = this.data.startPoint.clientY - this.data.dotPoint.clientY;
+    var startAngle = Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+    this.setData({
+      startAngle: startAngle
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  buttonMove: function (e) {
+    //获取滑动时的时间
+    var downTime = Date.now();
+    this.setData({
+      downTime: downTime
+    })
+    var that = this;
+    var endPoint = e.touches[e.touches.length - 1]
+    //根据触摸位置计算角度
+    var x = endPoint.clientX - this.data.dotPoint.clientX;
+    var y = endPoint.clientY - this.data.dotPoint.clientY;
+    var moveAngle = Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI
+    var quadrant = 1;
+    if (x >= 0) {
+      quadrant = y >= 0 ? 4 : 1;
+    } else {
+      quadrant = y >= 0 ? 3 : 2;
+    }
+    var tempAngle = 0;
+    // 如果是一、四象限，则直接end角度-start角度，角度值都是正值  
+    if (quadrant == 1 || quadrant == 4) {
+      tempAngle += moveAngle - this.data.startAngle;
+    } else
+    // 二、三象限，色角度值是负值  
+    {
+      tempAngle += this.data.startAngle - moveAngle;
+    }
+    var menuConfig = app.menuConfig.menu;
+    var menuList = [];
+    for (var i = 0; i < this.data.menuList.length; i++) {
+      menuList.push({ deg: this.data.menuList[i].deg + tempAngle, menu: menuConfig[i].menu, src: menuConfig[i].src });
+    }
+    this.setData({
+      menuList: menuList
+    })
+    //重置开始角度
+    this.setData({
+      startPoint: e.touches[e.touches.length - 1]
+    })
+    var endX = this.data.startPoint.clientX - this.data.dotPoint.clientX;
+    var endY = this.data.startPoint.clientY - this.data.dotPoint.clientY;
+    var startAngle = Math.asin(endY / Math.hypot(endX, endY)) * 180 / Math.PI;
+    this.setData({
+      startAngle: startAngle,
+      tempAngle: tempAngle
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  buttonEnd: function (e) {
+    // 计算，每秒移动的角度 
+    var that = this;
+    var upTime = Date.now();
+    var angleSpeed = this.data.tempAngle * 1000 / (upTime - this.data.downTime);
+    if (Math.abs(angleSpeed) < 100) {
+      //速度小于100时,停止滚动
+      return
+    } else {
+      //速度大于100时,自动滚动
+      if (angleSpeed > 0) {
+        if (angleSpeed > 500) angleSpeed = 500
+        var animationRun = wx.createAnimation({
+          duration: 2000,
+          //ease-out结束时减速
+          timingFunction: 'ease-out'
+        })
+        that.animationRun = animationRun
+        animationRun.rotate(angleSpeed).step()
+        that.setData({
+          animationData: animationRun.export(),
+        })
+      }
+      else {
+        if (angleSpeed < -500) angleSpeed = -500
+        angleSpeed = Math.abs(angleSpeed);
+        var animationRun = wx.createAnimation({
+          duration: 2000,
+          // ease-out结束时减速
+          timingFunction: 'ease-out'
+        })
+        that.animationRun = animationRun
+        animationRun.rotate(-angleSpeed).step()
+        that.setData({
+          animationData: animationRun.export(),
+        })
+      }
+    }
   }
 })
+
